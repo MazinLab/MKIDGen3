bada76bae1394ba57b47a18f672b1fc8c23c025c
Jeb Bailey <baileyji@ucsb.edu>
2020-05-02 09:47:44 -0700

    fixed point work

--- mkidgen3/fixedpoint.py
+++ mkidgen3/fixedpoint.py
@@ -13,9 +13,9 @@ FP16_17 = lambda x: FpBinary(int_bits=-1, frac_bits=17, signed=True, value=x)
 
 def do_fixed_point_pfb(fpcomb, fpcoeff, n_convert=None, truncate=True):
     """Set truncate to false to preserve the full output bitwidth. Truncation is done with FpBinary defaults."""
-    n_total_packets = fpcomb.size // 2048 if n_convert is None else n_convert
-    fft_block = np.zeros((n_total_packets + 1, 256, 16), dtype=np.complex64)
-    for i in range(0, 128, 2):  # each packet of ADC samples, 128 new things to a lane 2 packets to feed all channels
+    n_total_packets = fpcomb.size // 2048 // 2 - 16 if n_convert is None else n_convert
+    fft_block = np.zeros((n_total_packets+1, 256, 16), dtype=np.complex64)
+    for i in range(0, n_total_packets, 2):  # each packet of ADC samples, 128 new things to a lane 2 packets to feed all channels
         lane_out = np.zeros((2, 256, 16), dtype=np.complex64)
         for l in range(16):
             fresh = np.array([fpcoeff[l, :, 7 - c_i] * fpcomb[i + 2 * c_i:i + 2 * c_i + 2, l::16, :].reshape(256, 2).T
@@ -37,11 +37,28 @@ def do_fixed_point_pfb(fpcomb, fpcoeff, n_convert=None, truncate=True):
     return fft_block
 
 
-def fparray(a, fpgen):
-    """Convert a numpy array to an object array of FixedPoint numbers using the fp generator"""
-    out = np.empty_like(a, dtype=object)
-    if isinstance(type(a.dtype), (np.complex, np.complex128, np.complex64)):
-        out.flat = [(fpgen(x.real), fpgen(x.imag)) for x in a.flat]
+def fparray(a, fpgen=None):
+    """
+    Convert a numpy array to a numpy object array of FpBinary numbers.
+
+    Complex numbers gain an additional axis of length 2.
+
+    if fpgen is None 16wide  1integer signed is used
+    """
+    if fpgen is None:
+        fpgen = lambda x: FpBinary(int_bits=1, frac_bits=15, signed=True, value=x)
+
+    if a.dtype in (np.complex, np.complex128, np.complex64):
+        out = np.empty(tuple(list(a.shape)+[2]), dtype=object)
+        b = [(fpgen(x.real), fpgen(x.imag)) for x in a.flat]
+        for i in range(out.size):
+            out.flat[i] = b[i//2][i % 2]
+        #You'd think this would work, but it doesn't as the FP types get cast to bool
+        #out.flat = [(fpgen(x.real), fpgen(x.imag)) for x in a.flat]
     else:
-        out.flat = [fpgen(x) for x in a.flat]
+        out = np.empty_like(a, dtype=object)
+        b = [fpgen(x) for x in a.flat]
+        for i in range(out.size):
+            out.flat[i] = b[i]
+        #out.flat = [fpgen(x) for x in a.flat]
     return out
