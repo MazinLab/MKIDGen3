0865b32670dfa250f5a4287d81d86869f83cd393
Jenny Smith <jennifer_smith@ucsb.edu>
2021-09-23 15:27:13 -0700

    verified functionality

--- mkidgen3/drivers/dactable.py
+++ mkidgen3/drivers/dactable.py
@@ -15,7 +15,7 @@ class DACTableAXIM(DefaultIP):
 
     def replay(self, data, tlast=True, tlast_every=256, length=None, start=True, fpgen=lambda x: FP16_15(x)):
         """
-        data - an array of complex numbers, nominally 2^18 samples
+        data - an array of complex numbers, nominally 2^19 samples
         tlast - Set to true to emit a tlast pulse every tlast_every transactions
         length - Number of groups of 16 (DAC takes 16/clock) in the data to use.
             e.g. if data[N].reshape(N//16,16). Note that the user channel will count to length-1
@@ -23,24 +23,26 @@ class DACTableAXIM(DefaultIP):
         start - start the replay immediately
         """
         # Data has right shape
-        if data.size < 2 ** 18:
+        if data.size < 2 ** 19:
             getLogger(__name__).warning('Insufficient data, padding with zeros')
-            x = np.zeros(2 ** 18, dtype=np.complex64)
+            x = np.zeros(2 ** 19, dtype=np.complex64)
             x[:data.size] = data
             data = x
 
-        if data.size > 2 ** 18:
-            getLogger(__name__).warning('Data will be truncated to 2^18 samples')
-            data = data[:2 ** 18]
+        if data.size > 2 ** 19:
+            getLogger(__name__).warning('Data will be truncated to 2^19 samples')
+            data = data[:2 ** 19]
 
         # Process Length
         if length is None:
-            length = data.size // 8
+            length = data.size // 16
         length = int(length)
-        if length > 2 ** 15:  # NB 15 as length is in sets of 8
-            getLogger(__name__).warning('Clipping replay length to 2^15 sets of 8')
-        if length < 1:
-            raise ValueError('Replay length must be at least 1')
+
+        if length > 2 ** 15:  # NB 15 as length is in sets of 16
+            getLogger(__name__).warning('Clipping replay length to 2^15 sets of 16')
+            length = 2**15
+        if length % 2:
+            raise ValueError('Replay length must be a multiple of 2')
 
         # Process tlast_every
         tlast_every = int(tlast_every)
@@ -51,13 +53,16 @@ class DACTableAXIM(DefaultIP):
             tlast_every = 256  # just assign some value to ensure we didn't get handed garbage
 
         self._buffer = allocate(2 ** 20, dtype=np.uint16)
-        self._buffer[:data.size * 2:2] = [fpgen(x) for x in data.real] if fpgen is not None else data.real
-        self._buffer[1:data.size * 2:2] = [fpgen(x) for x in data.imag] if fpgen is not None else data.imag
+        iload = [fpgen(x).__index__() for x in data.real] if fpgen is not None else data.real
+        qload = [fpgen(x).__index__() for x in data.imag] if fpgen is not None else data.imag
+        for i in range(16):
+            self._buffer[i:data.size * 2:32] = iload[i::16]
+            self._buffer[i+16:data.size * 2:32] = qload[i::16]
 
-        self.register_map.a1 = self._buffer.device_address
-        self.register_map.length_V = length
+        self.register_map.a_1 = self._buffer.device_address
+        self.register_map.length_r = length-1  # length counter
         self.register_map.tlast = bool(tlast)
-        self.register_map.replay_length_V = tlast_every
+        self.register_map.replay_length = tlast_every
         self.register_map.run = True
         if start:
             self.register_map.CTRL.AP_START = 1
