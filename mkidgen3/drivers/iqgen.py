94cd42abc36e6842dbb2cfd1add12d51c0c0b5bb
Jeb Bailey <baileyji@ucsb.edu>
2022-02-17 21:44:46 -0800

    update iqgen driver

--- mkidgen3/drivers/iqgen.py
+++ mkidgen3/drivers/iqgen.py
@@ -1,45 +1,16 @@
-from pynq import allocate, DefaultIP
+from pynq import DefaultIP
 
 
 class IQGen(DefaultIP):
-    """
-    // control
-    // 0x00 : Control signals
-    //        bit 0  - ap_start (Read/Write/COH)
-    //        bit 1  - ap_done (Read/COR)
-    //        bit 2  - ap_idle (Read)
-    //        bit 3  - ap_ready (Read)
-    //        bit 7  - auto_restart (Read/Write)
-    //        others - reserved
-    // 0x04 : Global Interrupt Enable Register
-    //        bit 0  - Global Interrupt Enable (Read/Write)
-    //        others - reserved
-    // 0x08 : IP Interrupt Enable Register (Read/Write)
-    //        bit 0  - enable ap_done interrupt (Read/Write)
-    //        bit 1  - enable ap_ready interrupt (Read/Write)
-    //        others - reserved
-    // 0x0c : IP Interrupt Status Register (Read/TOW)
-    //        bit 0  - ap_done (COR/TOW)
-    //        bit 1  - ap_ready (COR/TOW)
-    //        others - reserved
-    // 0x10 : Data signal of max
-    //        bit 31~0 - max[31:0] (Read/Write)
-    // 0x14 : reserved
-    // (SC = Self Clear, COR = Clear on Read, TOW = Toggle on Write, COH = Clear on Handshake)
-    """
-    ADDR_CAPTURESIZE = 0x10
-    bindto = ['mazinlab:mkidgen3:iq_gen:0.1']
+    bindto = ['mazinlab:mkidgen3:iq_gen:0.5']
 
     def __init__(self, description):
         super().__init__(description=description)
 
-    def generate(self, n, start=False):
-        """
-        Tell the block to generate n sets of 8 IQ samples.
-        """
-        self.write(self.ADDR_CAPTURESIZE, n)
-        if start:
-            self.start()
+    def generate(self, n=2**20-1):
+        """ Tell the block to generate n multiples of 4096 of samples. """
+        self.register_map.max = n
+        self.register_map.run = True
 
-    def start(self):
-        self.register_map.CTRL.AP_START=1
+    def stop(self):
+        self.register_map.run=False
