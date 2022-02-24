331e48734910bbacebef589b497f62907e38b31e
Jeb Bailey <baileyji@ucsb.edu>
2022-02-24 12:07:16 -0800

    add anritsu helper

--- mkidgen3/util.py
+++ mkidgen3/util.py
@@ -2,6 +2,12 @@ import pkg_resources
 import os
 import yaml
 import logging.config
+import requests
+
+
+def set_anritsu(f):
+    """NB make sure Anritsu server is running"""
+    requests.get(f'http://blacklab.physics.ucsb.edu:51111/loset/{f}')
 
 
 def setup_logging(name):
