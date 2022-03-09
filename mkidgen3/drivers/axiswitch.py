c284e594b890e5d3300c955bcabe7b9c9d1603a3
Jeb Bailey <baileyji@ucsb.edu>
2022-03-08 21:32:20 -0800

    Tidy switch drivers

--- mkidgen3/drivers/axiswitch.py
+++ mkidgen3/drivers/axiswitch.py
@@ -8,7 +8,7 @@ class AxisSwitch(DefaultIP):
         super().__init__(description=description)
 
     def set_driver(self, slave=0, master=0, disable=False, commit=True):
-        """Set the slave for the master"""
+        """Set the slave for the master, committing the change triggers a 16 cycle soft reset of the switch"""
         cfg = slave & 0b111
         master = min(max(int(master), 0), 15)
         if disable:
@@ -17,10 +17,26 @@ class AxisSwitch(DefaultIP):
         if commit:
             self.commit()
 
-    def is_disabled(self, master=0):
-        return (self.read(0x0040 + min(max(int(master), 0), 15) * 4) & 0xf0000000) > 0
+    def disable(self, master=0):
+        """Disable the master, do not change the driver, will commit and soft-reset the core"""
+        master = min(max(int(master), 0), 15)
+        addr = 0x0040 + master * 4
+        self.write(addr, self.read(addr) | 0x80000000)
+        self.commit()
 
-    def driver_for(self, master=0):
+    def enable(self, master=0):
+        """Enable the master, do not change the driver, will commit and soft-reset the core"""
+        master = min(max(int(master), 0), 15)
+        addr = 0x0040 + master * 4
+        self.write(addr, self.read(addr) ^ 0x80000000)
+        self.commit()
+
+    def is_disabled(self, master=0):
+        """True iff the master is disabled"""
+        return (self.read(0x0040 + min(max(int(master), 0), 15) * 4) & 0xf0000000) != 0
+
+    def driver_of(self, master=0):
+        """Return the driver of the specified master, master may be disabled"""
         return self.read(0x0040 + min(max(int(master), 0), 15) * 4)
 
     def commit(self):
@@ -34,21 +50,25 @@ class SwitchOnLast(DefaultIP):
     def __init__(self, description):
         super().__init__(description=description)
 
-    def set_driver(self, slave=0, master=0, disable=False, commit=True):
-        """Set the slave for the master"""
+    def set_driver(self, slave=0, disable=False):
+        """Set the slave for the output"""
         slave = max(min(slave, 5), 0)
         if disable:
             self.register_map.enable = False
         self.register_map.stream = slave
-        if commit:
-            self.commit()
 
-    def is_disabled(self, master=0):
+    def disable(self):
+        """Disable the output"""
+        self.register_map.enable = False
+
+    def enable(self):
+        """Enable the output"""
+        self.register_map.enable = True
+
+    def is_disabled(self):
+        """True iff the output is disabled"""
         return self.register_map.enable.enable
 
-    def driver_for(self, master=0):
+    def driver_of(self):
+        """Return the driver for the output, output may be disabled"""
         return self.register_map.stream.stream
-
-    def commit(self):
-        """Commit config, triggers a soft 16 cycle reset"""
-        self.register_map.enable=True
