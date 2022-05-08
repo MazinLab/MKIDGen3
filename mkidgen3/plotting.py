0da776d42d52a65dbe796390efededa1c532d78d
Jeb Bailey <baileyji@ucsb.edu>
2022-05-07 23:49:35 -0400

    lots of plotting work

--- mkidgen3/plotting.py
+++ mkidgen3/plotting.py
@@ -1,7 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plt_adc_timeseries(data, start, stop, fs=4.096e9, ax=None, **kwargs):
+
+# NB requires a newer version than we have by default in pynq2.7 venv
+# def adc_test_plot(adc_data, timerange, fft_range, fft_zoom,  fs=4.096e9, figsize=(13, 10), **mosaic_kw):
+#     gs_kw = dict(width_ratios=[1, 1], height_ratios=[1, 1.4])
+#     fig, axd = plt.subplot_mosaic([['adc', 'adc'],
+#                                    ['fft', 'fftzoom']],
+#                                   gridspec_kw=gs_kw, figsize=figsize,
+#                                   constrained_layout=True, **mosaic_kw)
+#
+#
+#     adc_timeseries(adc_data, timerange, fs=fs, ax=axd['adc'])
+#
+#     fft_start, fft_stop = fft_range
+#     fft_sl = slice(*fft_range)
+#     fft_freqs = np.linspace(-fs/2, fs/2, fft_stop - fft_start)
+#     y_fft = np.abs(np.fft.fftshift(np.fft.fft(adc_data[fft_sl])))
+#
+#     plot_fft(fft_freqs[::2], y_fft[::2] - max(y_fft), ax=axd['fft'])
+#     plot_fft(fft_freqs[::2], y_fft[::2] - max(y_fft), ax=axd['fftzoom'], xlim=fft_zoom)
+#
+#     fig.suptitle('ADC Data')
+
+
+def adc_test_plot(adc_data, timerange, fft_range, fft_zoom,  fs=4.096e9, figsize=(13, 8), **mosaic_kw):
+    fig = plt.figure(figsize=figsize, constrained_layout=True)
+    spec = fig.add_gridspec(2, 2, width_ratios=[1, 1], height_ratios=[1, 1.8])
+    adcax = fig.add_subplot(spec[0, :])
+    fftax = fig.add_subplot(spec[1, 0])
+    fftzoomax = fig.add_subplot(spec[1, 1])
+
+    adc_timeseries(adc_data, timerange, fs=fs, ax=adcax)
+
+    fft_start, fft_stop = fft_range
+    fft_sl = slice(*fft_range)
+    fft_freqs = np.linspace(-fs/2, fs/2, fft_stop - fft_start)
+    y_fft = np.abs(np.fft.fftshift(np.fft.fft(adc_data[fft_sl])))
+
+    plot_fft(fft_freqs[::2], y_fft[::2] - max(y_fft), ax=fftax)
+    plot_fft(fft_freqs[::2], y_fft[::2] - max(y_fft), ax=fftzoomax, xlim=fft_zoom)
+
+    fig.suptitle('ADC Data')
+
+
+def adc_timeseries(data, timerange=(None, None), fs=4.096e9, ax=None, **kwargs):
     """
     data: np array of captured adc data
     start: first sample # in plot
@@ -11,21 +54,37 @@ def plt_adc_timeseries(data, start, stop, fs=4.096e9, ax=None, **kwargs):
     Returns:
         Plot of ADC timeseries.
     """
-    
+
     if ax is None:
-        ax = plt.gca()
+        plt.figure(figsize=(10, 5))
+    else:
+        plt.sca(ax)
 
-    n = data.shape[0] # total samples
-    tvec = np.linspace(0, n/fs, n)*1e9 # time vector [nano seconds]
-    sample_sl =  slice(start, stop) # plt slice
-
-    plt.figure(figsize=(10,5))
-    plt.plot(tvec[sample_sl], data.real[sample_sl])
-    plt.plot(tvec[sample_sl], data.real[sample_sl],"o")
+    n = data.shape[0]  # total samples
+    tvec = np.linspace(0, n/fs, n)*1e9  # time vector [nano seconds]  TODO only generate used samples
+    sl = slice(*timerange)  # plt slice
+    plt.plot(tvec[sl], data.real[sl])
+    plt.plot(tvec[sl], data.real[sl], "o")
     plt.grid(True)
-    plt.xlabel("time(ns)", position=(0.5,1))
-    plt.ylabel("signal(V)", position=(0,0.5))
-    ax.set_xlim(tvec[start], tvec[stop])
+    plt.xlabel("time (ns)", position=(0.5, 1))
+    plt.ylabel("signal", position=(0, 0.5))
+
+    plt.gca().set_xlim(None if timerange[0] is None else tvec[timerange[0]],
+                       None if timerange[1] is None else tvec[timerange[1]])
     plt.title('Time Series')
-    
-    
+
+
+def plot_fft(f, y, log=False, xlim=(-2.048e9, 2.048e9), ylim=None, ax=None):
+    if ax is not None:
+        plt.sca(ax)
+    plt.plot(f, y)
+    plt.grid(True)
+    if log:
+        plt.yscale("log")
+    if ylim is not None:
+        plt.ylim(*ylim)
+    if xlim is not None:
+        plt.xlim(*xlim)
+    plt.xlabel("Frequency (Hz)", position=(0.5, 0.5))
+    plt.ylabel("power", position=(1, 0.5))
+    plt.title('FFT')
