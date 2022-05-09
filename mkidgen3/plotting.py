e042c081b2d603f07466ba9502b9e6c074f7cec2
Jenny Smith <jennifer_smith@ucsb.edu>
2022-05-08 18:21:27 -0700

    updated plot labels and notebook documentation

--- mkidgen3/plotting.py
+++ mkidgen3/plotting.py
@@ -24,7 +24,7 @@ import numpy as np
 #     fig.suptitle('ADC Data')
 
 
-def adc_test_plot(adc_data, timerange, fft_range, fft_zoom,  fs=4.096e9, figsize=(13, 8), **mosaic_kw):
+def adc_test_plot(adc_data, timerange, fft_range, fft_zoom,  db=True, fs=4.096e9, figsize=(13, 8), **mosaic_kw):
     fig = plt.figure(figsize=figsize, constrained_layout=True)
     spec = fig.add_gridspec(2, 2, width_ratios=[1, 1], height_ratios=[1, 1.8])
     adcax = fig.add_subplot(spec[0, :])
@@ -37,6 +37,10 @@ def adc_test_plot(adc_data, timerange, fft_range, fft_zoom,  fs=4.096e9, figsize
     fft_sl = slice(*fft_range)
     fft_freqs = np.linspace(-fs/2, fs/2, fft_stop - fft_start)
     y_fft = np.abs(np.fft.fftshift(np.fft.fft(adc_data[fft_sl])))
+
+
+    if db:
+        y_fft = 20*np.log10(y_fft)
 
     plot_fft(fft_freqs[::2], y_fft[::2] - max(y_fft), ax=fftax)
     plot_fft(fft_freqs[::2], y_fft[::2] - max(y_fft), ax=fftzoomax, xlim=fft_zoom)
@@ -67,24 +71,24 @@ def adc_timeseries(data, timerange=(None, None), fs=4.096e9, ax=None, **kwargs):
     plt.plot(tvec[sl], data.real[sl], "o")
     plt.grid(True)
     plt.xlabel("time (ns)", position=(0.5, 1))
-    plt.ylabel("signal", position=(0, 0.5))
+    plt.ylabel("signal (V)", position=(0, 0.5))
 
     plt.gca().set_xlim(None if timerange[0] is None else tvec[timerange[0]],
                        None if timerange[1] is None else tvec[timerange[1]])
     plt.title('Time Series')
 
 
-def plot_fft(f, y, log=False, xlim=(-2.048e9, 2.048e9), ylim=None, ax=None):
+def plot_fft(f, y, db=True, xlim=(-2.048e9, 2.048e9), ylim=None, ax=None):
     if ax is not None:
         plt.sca(ax)
     plt.plot(f, y)
     plt.grid(True)
-    if log:
-        plt.yscale("log")
     if ylim is not None:
         plt.ylim(*ylim)
     if xlim is not None:
         plt.xlim(*xlim)
     plt.xlabel("Frequency (Hz)", position=(0.5, 0.5))
-    plt.ylabel("power", position=(1, 0.5))
+    plt.ylabel("power (linear)", position=(1, 0.5))
+    if db:
+        plt.ylabel("power (dB)", position=(1, 0.5))
     plt.title('FFT')
