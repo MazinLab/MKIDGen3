c069beebc1a1f029fba89db2e039ff13c94fba75
Jeb Bailey <baileyji@ucsb.edu>
2022-11-11 15:40:28 -0800

    more remote capture example word

--- mkidgen3/server.py
+++ mkidgen3/server.py
@@ -5,8 +5,10 @@ from .util import setup_logging
 import mkidgen3 as gen3
 import numpy as np
 import blosc
+import requests, tempfile
 
 SERVER_PORT = 50001
+CHUNK_SIZE = 8192
 
 
 def compress_array_chunks(data):
@@ -16,22 +18,31 @@ def compress_array_chunks(data):
 
 
 def serve_compressed_array(a, name):
-    return Response(
-        stream_with_context(compress_array_chunks(a)),
-        headers={
-            'Content-Disposition': f'attachment; filename={name}'
-        }
-    )
+    return Response(blosc.pack_array(a),
+                    headers={
+                        'Content-Disposition': f'attachment; filename={name}'
+                    }
+                )
+    # return Response(
+    #     stream_with_context(compress_array_chunks(a)),
+    #     headers={
+    #         'Content-Disposition': f'attachment; filename={name}'
+    #     }
+    # )
 
 
-class IQCaptureAPI(Resource):
-    def get(self, n_samples):
+class CaptureAPI(Resource):
+    def get(self, tap, n_samples):
         try:
             n_samples=int(n_samples)
             print(f'caturing {n_samples}')
             getLogger(__name__).info(f'caturing {n_samples}')
             # data = list(np.arange(n_samples)/n_samples)
-            data = gen3.iqcapture(int(n_samples))
+            if tap=='iq':
+                data = gen3.iqcapture(int(n_samples))
+            elif tap=='adc':
+                pass
+            else
             return serve_compressed_array(data, 'foo.np')
         except RuntimeError:
             return 'Error: did you set frequencies?', 400
@@ -47,23 +58,27 @@ class IQConfigAPI(Resource):
             getLogger(__name__).info(f'Bad frequencies: {request.json}')
             return 'Bad JSON data', 400
 
-setup_logging('gen3-flask')
-app = Flask(__name__, static_url_path="")
-api = Api(app)
-api.add_resource(IQConfigAPI, '/set_freq')
-api.add_resource(IQCaptureAPI, '/iqcap/<n_samples>')
-# app.run(host='0.0.0.0', debug=False, port=SERVER_PORT)
 
-import requests, tempfile
-def get_data():
+def remote_capture(tap, nsamples, url=f'http://mkidzcu111b.physics.ucsb.edu:{SERVER_PORT}'):
     def fetch_blosc_array(url):
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
             with tempfile.TemporaryFile(mode='w+b') as tf:
-                for chunk in r.iter_content(chunk_size=8192):
+                for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
                     tf.write(chunk)
                 tf.seek(0)
                 array = blosc.unpack_array(tf)
         return array
 
-    return fetch_blosc_array('{url}/capture/{source}/{nsamples}')
+    return fetch_blosc_array(f'{url}/cap/{tap}/{nsamples}')
+
+
+if __name__=='__main__':
+    gen3.configure(.....)
+    setup_logging('gen3-flask')
+    app = Flask(__name__, static_url_path="")
+    api = Api(app)
+    api.add_resource(IQConfigAPI, '/set_freq')
+    api.add_resource(CaptureAPI, '/cap/<tap>/<n_samples>')
+    # api.add_resource(IQCaptureAPI, '/adccap/<n_samples>')
+    app.run(host='0.0.0.0', debug=False, port=SERVER_PORT)
