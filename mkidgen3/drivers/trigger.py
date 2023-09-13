7f3f38e91954dcb64826ccedde7da401a9de1ff7
Aled Cuda <aled@ucsb.edu>
2023-09-13 18:54:14 +0000

    Fix duplicated I stream in PostageMAXI capture

--- mkidgen3/drivers/trigger.py
+++ mkidgen3/drivers/trigger.py
@@ -196,7 +196,7 @@ class PhotonPostageMAXI(DefaultIP):
         ids = data[:, 0, 0].astype(np.uint16)
         events = data[:, 1:, :]
         if not raw:
-            events = events[:, :, 0] + events[:, :, 0] * 1j
+            events = events[:, :, 0] + events[:, :, 1] * 1j
             if scaled:
                 events /= 2 ** 14
         return ids, events
