ab52ff9c9da1a2d1d87650d2994066b02d42660f
Aled Cuda <aled@ucsb.edu>
2023-09-13 18:00:56 +0000

    import all the drivers we actually use

--- mkidgen3/drivers/__init__.py
+++ mkidgen3/drivers/__init__.py
@@ -1,6 +1,6 @@
 try:
-    from . import axiswitch, bintores, capture, ddc, dactable, axififo, phasematch, iqgen, rfdc
+    from . import axiswitch, bintores, capture, ddc, dactable, axififo, phasematch, iqgen, rfdc, trigger
 except OSError:
     pass
 __all__ = ['axiswitch', 'bintores', 'capture', 'ddc', 'dactable', 'axififo', 'rfdc', 'iqgen',
-           'phasematch']
+           'phasematch', 'trigger']
