ca189c6115905becda448a5931d500205a854057
Jenny Smith <jennifer_smith@ucsb.edu>
2023-10-04 11:06:16 -0700

    more work towards power sweep board test

--- arraysetup/sweep_test.py
+++ arraysetup/sweep_test.py
@@ -1,6 +1,6 @@
-#import zmq
-#from mkidgen3.server.feedline_client_objects import CaptureRequest, CaptureJob, FeedlineConfig
-from mkidgen3.server.feedline_objects import IFConfig, DACConfig
+import zmq
+from mkidgen3.server.feedline_client_objects import CaptureRequest, CaptureJob
+from mkidgen3.server.feedline_objects import IFConfig, DACConfig, PhotonPipeConfig, ChannelConfig, DDCConfig, FeedlineConfig
 
 
 #ctx = zmq.Context.instance()
@@ -14,11 +14,16 @@ feedline_server = 'tcp://localhost:8888'
 capture_data_server = 'tcp://localhost:8889'
 status_server = 'tcp://localhost:8890'
 
-ifconfig = IFConfig(lo=3000, adc_attn=20, dac_attn=20)
-dacconfig = DACConfig(n_uniform_tones=512)
+if_config = IFConfig(lo=3000, adc_attn=20, dac_attn=20)
+dac_config = DACConfig(n_uniform_tones=512)
+
+frequencies = dac_config._waveform.freqs
+
+chan_config = ChannelConfig(frequencies)
+ddc_config = DDCConfig(tones=frequencies)
+pp_config = PhotonPipeConfig(chan_config, ddc_config)
+
+fl_config = FeedlineConfig(if_config, dac_config, pp_config)
 
 
-
-#fc = FeedlineConfig(ifconfig,)
-
-#test_cr = CaptureRequest(1024, 'adc', fc, feedline_server)
+#test_cr = CaptureRequest(1024, 'adc', fl_config, feedline_server)
