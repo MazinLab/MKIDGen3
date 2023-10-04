e0ea8795faeadc4fccf24ae85b218c9753c7233c
Jenny Smith <jennifer_smith@ucsb.edu>
2023-10-04 14:36:45 -0700

    updated network address

--- arraysetup/sweep_test.py
+++ arraysetup/sweep_test.py
@@ -1,5 +1,5 @@
 import zmq
-from mkidgen3.server.feedline_client_objects import CaptureRequest, CaptureJob
+from mkidgen3.server.feedline_objects import CaptureRequest, FRSClient
 from mkidgen3.server.feedline_objects import IFConfig, DACConfig, PhotonPipeConfig, ChannelConfig, DDCConfig, FeedlineConfig
 
 
@@ -10,9 +10,9 @@ from mkidgen3.server.feedline_objects import IFConfig, DACConfig, PhotonPipeConf
 # cap data 8889
 # cap status 9000
 
-feedline_server = 'tcp://localhost:8888'
-capture_data_server = 'tcp://localhost:8889'
-status_server = 'tcp://localhost:8890'
+feedline_server = 'tcp://rfsoc4x2b.physics.ucsb.edu:8888'
+capture_data_server = 'tcp://rfsoc4x2b.physics.ucsb.edu:8889'
+status_server = 'tcp://rfsoc4x2b.physics.ucsb.edu:8890'
 
 if_config = IFConfig(lo=3000, adc_attn=20, dac_attn=20)
 dac_config = DACConfig(n_uniform_tones=512)
@@ -26,4 +26,4 @@ pp_config = PhotonPipeConfig(chan_config, ddc_config)
 fl_config = FeedlineConfig(if_config, dac_config, pp_config)
 
 
-#test_cr = CaptureRequest(1024, 'adc', fl_config, feedline_server)
+test_cr = CaptureRequest(1024, 'adc', fl_config, feedline_server)
