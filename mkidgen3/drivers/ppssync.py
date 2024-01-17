5402b191c5ee67cd23bbce26e7984e477e98c162
Aled Cuda <aled@ucsb.edu>
2024-01-17 07:30:41 +0000

    Update PPSSync driver to use new register api

--- mkidgen3/drivers/ppssync.py
+++ mkidgen3/drivers/ppssync.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import time
 
+import mkidgen3.util as util
+
 from enum import Enum, IntFlag
 from math import floor
 
@@ -116,12 +118,6 @@ class PPSSync(DefaultIP):
         super().__init__(description=description)
         self.capture_event = asyncio.Event()
         self.started_event = asyncio.Event()
-        self._shadow_mode = 0
-        self._shadow_counter_config = 0
-        self._shadow_delay_ns = 0
-        self._shadow_rollover = 0
-        self._shadow_lockout = 0
-        self._shadow_load_time = [0, 0, 0]
 
     # High-level api
     async def start_engine(
@@ -333,151 +329,121 @@ class PPSSync(DefaultIP):
         self.stop_engine()
         return pps_time % NS_PER_SEC
 
-    # Individual register fields, quasi user-accessible
-    @property
-    def started(self):
-        """Is 1 if the PPS counter engine is started, 0 otherwise"""
-        return bool(_field_get(self.register_map.counter_status_reg.counter_status_reg, 0, 1))
+    # Base Registers
+    @util.register_ro
+    def _counter_status_reg(self):
+        return self.register_map.counter_status_reg
 
-    @property
-    def captured(self):
-        """Us 1 if the capture engine has captured, 0 otherwise"""
-        return bool(_field_get(self.register_map.counter_status_reg.counter_status_reg, 8, 1))
-
-    @property
-    def mode(self):
-        """This is the PPS Counter engines mode, of type :py:class:`mkidgen3.drivers.ppssync.PPSMode`"""
-        return PPSMode(_field_get(self._mode_reg, 0, 0b1111))
-
-    @mode.setter
-    def mode(self, value):
-        self._mode_reg = _field_set(self._mode_reg, 0, 0b1111, value.value)
-
-    @property
-    def capture_mode(self):
-        """The Capture Mode of the capture engine, of type :py:class:`mkidgen3.drivers.ppssync.CaptureMode`"""
-        return CaptureMode(_field_get(self._mode_reg, 8, 0b11))
-
-    @capture_mode.setter
-    def capture_mode(self, value):
-        self._mode_reg = _field_set(self._mode_reg, 8, 0b11, value.value)
-
-    @property
-    def sanity_mode(self):
-        """The Sanity Mode of the counter engine, of type :py:class:`mkidgen3.drivers.ppssync.SanityMode`"""
-        return SanityMode(_field_get(self._mode_reg, 16, 0b111))
-
-    @sanity_mode.setter
-    def sanity_mode(self, value):
-        self._mode_reg = _field_set(self._mode_reg, 16, 0b111, value.value)
-
-    @property
-    def pps_source(self):
-        return PPSSource(_field_get(self._mode_reg, 24, 0b111))
-
-    @pps_source.setter
-    def pps_source(self, value):
-        self._mode_reg = _field_set(self._mode_reg, 24, 0b111, value.value)
-
-    @property
-    def ns_per_clk(self):
-        return _field_get(self._counter_config, 8, 0b111111)
-
-    @ns_per_clk.setter
-    def ns_per_clk(self, value):
-        self._counter_config = _field_set(self._counter_config, 8, 0b111111, value)
-
-    @property
-    def subns_per_clk(self):
-        return _field_get(self._counter_config, 0, 0xFF)
-
-    @subns_per_clk.setter
-    def subns_per_clk(self, value):
-        self._counter_config = _field_set(self._counter_config, 0, 0xFF, value)
-
-    # Shadow Registers
-    @property
+    @util.register_shadow(0)
     def _mode_reg(self):
         """Provides a read write interface to the underlying mode register
         which is write-only, should not be called by user code as _mode_reg
         contains several config registers one of which is also confusingly
         what is set by PPSSync.mode, I'm very sorry."""
-        return self._shadow_mode
+        return self.register_map.mode_reg
 
-    @_mode_reg.setter
-    def _mode_reg(self, value):
-        self._shadow_mode = value
-        self.register_map.mode_reg.mode_reg = value
+    @util.register_shadow(0)
+    def _counter_config_reg(self):
+        return self.register_map.counter_config_reg
 
-    @property
-    def _counter_config(self):
-        """See PPSSynch._mode_reg"""
-        return self._shadow_counter_config
-
-    @_counter_config.setter
-    def _counter_config(self, value):
-        self._shadow_counter_config = value
-        self.register_map.counter_config_reg.counter_config_reg = value
-
-    @property
+    # Quasi user-accessible registers
+    @util.register_shadow(0)
     def lockout(self):
         """The lockout interval in ns (See :py:class:`mkidgen3.drivers.ppssync.SanityMode`)"""
-        return self._shadow_lockout
+        return self.register_map.lockout_reg
 
-    @lockout.setter
-    def lockout(self, value):
-        self._shadow_lockout = value
-        self.register_map.lockout_reg.lockout_reg = value
-
-    @property
+    @util.register_shadow(0)
     def rollover_thresh(self):
         """The rollover threshold in ns (See :py:class:`mkidgen3.drivers.ppssync.SanityMode`)"""
-        return self._shadow_rollover
+        return self.register_map.rollover_thresh_reg
 
-    @rollover_thresh.setter
-    def rollover_thresh(self, value):
-        self._shadow_rollover = value
-        self.register_map.rollover_thresh_reg.rollover_thresh_reg = value
-
-    @property
+    @util.register_shadow(0)
     def load_secs(self):
         """The seconds counter value to load when we start the PPS engine"""
-        return self._shadow_load_time[0]
+        return self.register_map.load_secs_reg
 
-    @load_secs.setter
-    def load_secs(self, value):
-        self._shadow_load_time[0] = value
-        self.register_map.load_secs_reg.load_secs_reg = value
-
-    @property
+    @util.register_shadow(0)
     def load_ns(self):
         """The ns counter value to load when we start the PPS engine"""
-        return self._shadow_load_time[1]
+        return self.register_map.load_ns_reg
 
-    @load_ns.setter
-    def load_ns(self, value):
-        self._shadow_load_time[1] = value
-        self.register_map.load_ns_reg.load_ns_reg = value
-
-    @property
+    @util.register_shadow(0)
     def load_subns(self):
         """The subns counter value to load when we start the PPS engine"""
-        return self._shadow_load_time[2]
+        return self.register_map.load_subns_reg
 
-    @load_subns.setter
-    def load_subns(self, value):
-        self._shadow_load_time[2] = value
-        self.register_map.load_subns_reg.load_subns_reg = value
-
-    @property
+    @util.register_shadow(0)
     def delay_ns(self):
-        """The ns counter value to load when we start the PPS engine"""
-        return self._shadow_delay_ns
+        """The time in ns to delay each PPS Edge"""
+        return self.register_map.delay_ns_reg
 
-    @delay_ns.setter
-    def delay_ns(self, value):
-        self._shadow_delay_ns = value
-        self.register_map.delay_ns_reg.delay_ns_reg = value
+    @util.register_ro
+    def captured_subns(self):
+        """The raw captured subns counter value"""
+        return self.register_map.capture_subns_reg
+
+    @util.register_ro
+    def captured_ns(self):
+        """The raw captured ns counter value"""
+        return self.register_map.capture_ns_reg
+
+    @util.register_ro
+    def captured_secs(self):
+        """The raw captured secs counter value"""
+        return self.register_map.capture_secs_reg
+
+    @util.register_ro
+    def subns(self):
+        """The raw current subns counter value"""
+        return self.register_map.current_subns_reg
+
+    @util.register_ro
+    def ns(self):
+        """The raw current ns counter value"""
+        return self.register_map.current_ns_reg
+
+    @util.register_ro
+    def secs(self):
+        """The raw current secs counter value"""
+        return self.register_map.current_secs_reg
+
+    # Individual register fields, quasi user-accessible
+    @util.field(_counter_status_reg)
+    def started(self):
+        """Is 1 if the PPS counter engine is started, 0 otherwise"""
+        return 0
+
+    @util.field(_counter_status_reg)
+    def captured(self):
+        """Is 1 if the capture engine has captured, 0 otherwise"""
+        return 8
+
+    @util.field_enum(_mode_reg, PPSMode)
+    def mode(self):
+        """This is the PPS Counter engines mode, of type :py:class:`mkidgen3.drivers.ppssync.PPSMode`"""
+        return slice(0, 4)
+
+    @util.field_enum(_mode_reg, CaptureMode)
+    def capture_mode(self):
+        """The Capture Mode of the capture engine, of type :py:class:`mkidgen3.drivers.ppssync.CaptureMode`"""
+        return slice(8, 10)
+
+    @util.field_enum(_mode_reg, SanityMode)
+    def sanity_mode(self):
+        """The Sanity Mode of the counter engine, of type :py:class:`mkidgen3.drivers.ppssync.SanityMode`"""
+        return slice(16, 19)
+
+    @util.field_enum(_mode_reg, PPSSource)
+    def pps_source(self):
+        return slice(24, 27)
+
+    @util.field(_counter_config_reg)
+    def ns_per_clk(self):
+        return slice(8, 14)
+
+    @util.field(_counter_config_reg)
+    def subns_per_clk(self):
+        return slice(0, 8)
 
     # Timer readout stuffs
     @property
@@ -509,33 +475,3 @@ class PPSSync(DefaultIP):
     def current_time(self):
         """The current time according to the core in floating point seconds"""
         return self.current_time_ns / (1000 * 1000 * 1000)
-
-    @property
-    def captured_subns(self):
-        """The raw captured subns counter value"""
-        return self.register_map.capture_subns_reg.capture_subns_reg
-
-    @property
-    def captured_ns(self):
-        """The raw captured ns counter value"""
-        return self.register_map.capture_ns_reg.capture_ns_reg
-
-    @property
-    def captured_secs(self):
-        """The raw captured seconds counter value"""
-        return self.register_map.capture_secs_reg.capture_secs_reg
-
-    @property
-    def subns(self):
-        """The raw current subns counter value"""
-        return self.register_map.current_subns_reg.current_subns_reg
-
-    @property
-    def ns(self):
-        """The raw current ns counter value"""
-        return self.register_map.current_ns_reg.current_ns_reg
-
-    @property
-    def secs(self):
-        """The raw current seconds counter value"""
-        return self.register_map.current_secs_reg.current_secs_reg
