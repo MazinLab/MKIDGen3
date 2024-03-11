43b3aaa1a8bb2336fa04d4226071181d1aeb2928
Aled Cuda <aled@ucsb.edu>
2024-03-11 10:36:14 -0700

    Remove Cruft

--- cruft/make_default_phasematch_coe.py
+++
@@ -1,6 +0,0 @@
-def make_default_phase_filter(file):
-    x=[[0]*29+[-1]]*512
-    x=[z for y in x for z in y]
-    x=' '.join(map(str,x))
-    with open(file, 'w') as f:
-      f.write('radix=10;\ncoefdata={};'.format(x))
